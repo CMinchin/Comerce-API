65fc551a5679259dbc34f6c04c8a9170c16a07e1
Equinox <cameron.minchin@gmail.com>
2022-10-07 12:53:32 +0800

    added correct models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo( Category, {
+  foreighKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany( Product, {
+  foreighKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany( Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany( Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_id'
+});
 
 module.exports = {
   Product,
